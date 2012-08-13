9bdbfbbaf8135472ad512358397dd0d2df7322ad
631210001 <631210001@Sala603-01.educcbx.senacrs.com.br>
2012-08-13 20:45:48 -0300

    primeiro programa do zero

---
+++ entregas/aula2/src/main/java/br/com/senac/braian/aulas/aula2/App.java
@@ -0,0 +1,13 @@
+package br.com.senac.braian.aulas.aula2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
