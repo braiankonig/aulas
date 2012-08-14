ecd64c5f5a3ffaf92fe37315d2b26299b117ac0c
631210001 <631210001@Sala603-01.educcbx.senacrs.com.br>
2012-08-13 22:11:52 -0300

    Atualizadas as classes

---
+++ entregas/aula2/src/main/java/br/com/senac/braian/aulas/aula2/Funcionario.java
@@ -0,0 +1,16 @@
+package br.com.senac.braian.aulas.aula2;
+
+public class Funcionario extends Pessoa {
+	
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
+}
---
+++ entregas/aula2/src/main/java/br/com/senac/braian/aulas/aula2/Secretaria.java
@@ -0,0 +1,5 @@
+package br.com.senac.braian.aulas.aula2;
+
+public class Secretaria extends Funcionario {
+
+}
