e1d8eb8a4f2bf2c46224eb12ca43e52050551317
631210001 <631210001@Sala603-01.educcbx.senacrs.com.br>
2012-08-13 22:18:11 -0300

    exercicio terminado

--- entregas/aula2/src/main/java/br/com/senac/braian/aulas/aula2/Chefe.java
+++ entregas/aula2/src/main/java/br/com/senac/braian/aulas/aula2/Chefe.java
@@ -1,5 +1,17 @@
 package br.com.senac.braian.aulas.aula2;
 
-public class Chefe extends Funcionario{
+public class Chefe extends Funcionario {
+	
+	Secretaria Secretaria = new Secretaria();
+
+	public Secretaria getSecretaria() {
+		return Secretaria;
+	}
+
+	public void setSecretaria(Secretaria Secretaria) {
+		this.Secretaria = Secretaria;
+	}
+	
+	
 
 }
