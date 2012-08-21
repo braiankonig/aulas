e5dd56eb17ee5e55edfbf7a750949b77e073f138
631210001 <631210001@Sala603-01.educcbx.senacrs.com.br>
2012-08-20 22:35:35 -0300

    minhalista

---
+++ minhalista/src/main/java/br/com/senacrs/alp/aulas/minhalista/Nodo.java
@@ -0,0 +1,32 @@
+package br.com.senacrs.alp.aulas.minhalista;
+
+public class Nodo<Tipo extends Object> {
+	
+	private Tipo valor;
+	private Nodo<Tipo> proximo = null;
+	
+	public Nodo(Tipo valor2) {
+		
+		this.valor = valor;
+		
+	}
+	
+	public Tipo getValor() {
+		return valor;
+	}
+	
+	public void setValor(Tipo valor) {
+		this.valor = valor;
+	}
+	
+	public Nodo<Tipo> getProximo() {
+		return proximo;
+	}
+	
+	public void setProximo(Nodo<Tipo> proximo) {
+		this.proximo = proximo;
+	}
+	
+	
+
+}
