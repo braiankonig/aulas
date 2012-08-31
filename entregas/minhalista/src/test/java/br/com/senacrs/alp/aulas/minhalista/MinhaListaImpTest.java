3d5201fac9afe9137f1da79903ccf08d2770813b
631210001 <631210001@Sala603-01.educcbx.senacrs.com.br>
2012-08-31 19:26:05 -0300

    sufixar, prefixar

--- entregas/minhalista/src/test/java/br/com/senacrs/alp/aulas/minhalista/MinhaListaImpTest.java
+++ entregas/minhalista/src/test/java/br/com/senacrs/alp/aulas/minhalista/MinhaListaImpTest.java
@@ -1,6 +1,7 @@
 package br.com.senacrs.alp.aulas.minhalista;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
@@ -22,17 +23,32 @@ public class MinhaListaImpTest {
 		MinhaListaImp<String> obj = null;
 		String arg = null;
 		
+		arg = "valor valido";
 		obj = new MinhaListaImp<String>(arg);
+		Assert.assertNotNull(obj);		
+	}
+	
+	@Test
+	public void testMinhaListaImpNull() {
+		
+		MinhaListaImp<String> obj = null;
+		
+		try{
+			obj = new MinhaListaImp<String>(null);
+			fail();
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+		Assert.assertNull(obj);		
+	}
+
+	@Test
+	public void testGetInicio() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testSufixar() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPrefixar() {
+	public void testSetInicio() {
 		fail("Not yet implemented");
 	}
 
@@ -42,18 +58,102 @@ public class MinhaListaImpTest {
 	}
 
 	@Test
+	public void testPrefixar(){
+		double esperado = 0.0;
+		double resultado = 0.0;
+		int comparacao = 0;
+
+		int nro_elementos = 0;
+		
+		MinhaListaImp<String> obj = null;
+		String arg = null;
+		
+		arg = "valor inicial";
+		obj = new MinhaListaImp<String>(arg);
+		
+		arg = "valor para prefixar";
+		obj.prefixar(arg);
+		
+		nro_elementos = 2;
+		
+		esperado = nro_elementos;
+		resultado = obj.tamanho();
+		comparacao = Double.compare(resultado, esperado);
+		Assert.assertEquals(
+				"Passando-se dois elementos para a lista o tamanho deve ser " + String.valueOf(esperado) 
+				+ ", valor retornando: " + String.valueOf(resultado),
+				comparacao, 0);				
+	}
+	
+	@Test
+	public void testSufixar() {
+		double esperado = 0.0;
+		double resultado = 0.0;
+		int comparacao = 0;
+		
+		int nro_elementos = 0;
+		
+		MinhaListaImp<String> obj = null;
+		String arg = null;
+		
+		arg = "valor inicial";
+		obj = new MinhaListaImp<String>(arg);
+		
+		arg = "valor para sufixar";
+		obj.sufixar(arg);
+		
+		nro_elementos = 2;
+		
+		esperado = nro_elementos;
+		resultado = obj.tamanho();
+		comparacao = Double.compare(resultado, esperado);
+		Assert.assertEquals(
+				"Passando-se " + nro_elementos + " elementos para a lista o tamanho deve ser " + String.valueOf(esperado) 
+				+ ", valor retornando: " + String.valueOf(resultado),
+				comparacao, 0);				
+	}
+	
+	@Test
 	public void testInserir() {
 		fail("Not yet implemented");
 	}
-
+	
 	@Test
 	public void testRemover() {
-		fail("Not yet implemented");
+		fail("Not yet implemented");		
 	}
-
+	
 	@Test
 	public void testTamanho() {
-		fail("Not yet implemented");
+		double esperado = 0.0;
+		double resultado = 0.0;
+		int comparacao = 0;
+		
+		int nro_elementos = 0;
+		
+		MinhaListaImp<String> obj = null;
+		String arg = null;
+		
+		arg = "valor inicial";
+		obj = new MinhaListaImp<String>(arg);
+		nro_elementos += 1;
+		
+		arg = "valor para prefixar";
+		obj.prefixar(arg);
+		nro_elementos += 1;
+		
+		arg = "valor para sufixar";
+		obj.sufixar(arg);
+		nro_elementos += 1;
+		
+		esperado = nro_elementos;
+		resultado = obj.tamanho();
+		comparacao = Double.compare(resultado, esperado);
+		Assert.assertEquals(
+				"Passando-se  " + nro_elementos + " elementos para a lista o tamanho deve ser " + String.valueOf(esperado) 
+				+ ", valor retornando: " + String.valueOf(resultado),
+				comparacao, 0);		
+		
 	}
-
+	
 }
